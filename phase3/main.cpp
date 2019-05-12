9e9ebd4cce6bd13ad01f2eb39375dcc427f8cdc5
arzulti <arzulti@aligntech.com>
2019-05-12 19:54:26 +0300

    Compiling using VS code

--- phase3/main.cpp
+++ phase3/main.cpp
@@ -1,16 +1,18 @@
-// #include <emscripten.h>
-//#include "substitution_cipher.h"
+// #include "emscripten.h"
 #include <string>
+#include <iostream>
+#include "substitution_cipher.h"
 using namespace std; 
 
 // EM_JS(void, run_code, (const char* str), {
-//     eval(UTF8ToString(str));
+    //  eval(UTF8ToString(str));
 // });
 
 int main() {
-    string alpha("");
-    string key("");
-    string input("");
-    string code("");
-    //SubsetituionCipher.decrypt(input, key, alpha, code);
+	string alpha;
+    string key;
+    string input;
+    string code;
+    cout << "Hello";
+	SubstitutionCipher::decrypt(input, key, alpha, code);
 }
--- phase3/substitution_cipher.h
+++ phase3/substitution_cipher.h
@@ -1,10 +1,12 @@
-class SubsetituionCipher 
+#pragma once
+#include <string>
+
+using namespace std;
+
+class SubstitutionCipher 
 {
-    private:
-    static bool cipher(string input, string oldAlphabet, string newAlphabet, string &output);
-
-    public:
-    static bool encrypt(string input, string plainAlphabet, string cipherAlphabet, string &output);
-    static bool decrypt(string input, string plainAlphabet, string cipherAlphabet, string &output);
-
-}
+	static bool cipher(string input, string old_alphabet, string new_alphabet, string& output);
+public:
+	static bool encrypt(string input, string plainAlphabet, string cipherAlphabet, string &output);
+	static bool decrypt(string input, string plainAlphabet, string cipherAlphabet, string &output);
+};
