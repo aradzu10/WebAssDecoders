b9b6600dd1fd865c0aa9813da2c74c92fe7d32a9
arzulti <arzulti@aligntech.com>
2019-06-07 00:25:29 +0300

    phase 15 - completed

--- Phase15/main.cpp
+++ Phase15/main.cpp
@@ -4,12 +4,13 @@
 #include <emscripten/html5.h>
 #include <sstream>
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
 int rnd_f = 0;
 int rnd_s = 0;
-char code[] = "(function () {alert('Hello there, General Kanobi')})(";
+char code[] = "function () {alert('Hello there, General Kanobi')})(";
 
 EM_JS(void, run_code, (const char* str), {
     try {
@@ -27,8 +28,6 @@ EM_JS(int, get_window_height, (), {
 });
 
 EM_BOOL mouse_move(int eventType, const EmscriptenMouseEvent *e, void *userData) {
-    char table[] = {'1', 'a', ')', 'b', 'v', '(', 'd'};
-
     ostringstream oss("");
     int width = get_window_width(); 
     int index_f = ((e->screenX / (width / 7)) + rnd_f) % 7;
@@ -36,14 +35,18 @@ EM_BOOL mouse_move(int eventType, const EmscriptenMouseEvent *e, void *userData)
     int height = get_window_height(); 
     int index_s = ((e->screenY / (height / 7)) + rnd_s) % 7;
     
+    // for code to compile, need first = '(' = 40 second = ')' = 41
+    oss << (char) (40 + index_f);
     oss << code;
-    oss << table[index];
+    oss << (char) (40 + index_s);
     run_code(oss.str().c_str());
     
     return 0;
 }
 
 int main() {
+    srand(time(0));
+
     rnd_f = rand() % 7;
     rnd_s = rand() % 7;
     emscripten_set_mousemove_callback(0, 0, 1, mouse_move);
