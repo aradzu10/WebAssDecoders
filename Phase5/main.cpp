20bc22f82be12da7fa582a20c813e490f954cf56
arzulti <arzulti@aligntech.com>
2019-06-06 21:29:32 +0300

    start of phase 5

---
+++ Phase5/main.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <emscripten.h>
+#include <string>
+#include <emscripten/html5.h>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+int rnd_f = 0;
+int rnd_s = 0;
+char code[] = "(function () {alert('Hello there, General Kanobi')})(";
+
+EM_JS(void, run_code, (const char* str), {
+    new Function(UTF8ToString(str))();
+});
+
+int main() {
+  
+  ostringstream oss("");
+
+  FILE *file = fopen("../Phase5/code/code.txt", "rb");
+  if (!file) {
+    printf("cannot open file\n");
+    return 1;
+  }
+  do {
+    char c = fgetc(file);
+    oss << c;
+  } while(c != EOF);
+  fclose (file);
+
+  run_code(oss.str().c_str());
+
+  return 0;
+}
