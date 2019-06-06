d04f1e28b57603caa0a002080b331095af9cfd66
arzulti <arzulti@aligntech.com>
2019-06-07 00:13:59 +0300

    phase 14 - completed

--- Phase14/main.cpp
+++ Phase14/main.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <iostream>
 #include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
@@ -14,10 +15,6 @@ char code[] = "(function () {alert('Hello there, General Kanobi')})()";
 
 EM_JS(void, run_code, (const char* str), {
     new Function(UTF8ToString(str))();
-});
-
-EM_JS(void, logger, (long a, long b), {
-  console.log("width: " + a + ", height: " + b);
 });
 
 EM_JS(int, get_window_width, (), {
@@ -30,7 +27,6 @@ EM_JS(int, get_window_height, (), {
 
 EM_BOOL mouse_move(int eventType, const EmscriptenMouseEvent *e, void *userData) {
 
-  logger(e->clientX, e->clientY);
   if (abs(e->clientX - rnd_width) <= 50 && abs(e->clientY - rnd_height) <= 50) {
       run_code(code);
   }
@@ -38,13 +34,12 @@ EM_BOOL mouse_move(int eventType, const EmscriptenMouseEvent *e, void *userData)
 }
 
 int main() {
+    srand(time(0));
 
     int width = get_window_width() - 100;
     int height = get_window_height() - 100;
     rnd_width = (rand() % width) + 50;
     rnd_height = (rand() % height) + 50;
-    logger(width, height);
-    logger(rnd_width, rnd_height);
     emscripten_set_mousemove_callback(0, 0, 1, mouse_move);
     return 0;
 }
