e45f9cceb56b99f5c6857aedc2b7102261fcc1e5
mdombelski <mdombelski@aligntech.com>
2019-06-15 15:06:52 +0300

    phase 8 - done

--- phase8/src/main.cpp
+++ phase8/src/main.cpp
@@ -10,11 +10,11 @@ EM_JS(void, run_code, (const char* str), {
 });
 
 int main() {
-    int lenght = 36;
-    char words[14][10] = {"success", "wait", "pass", "break", "while do", "fail", "signed", "join", "namespace", "queue", "zero", "xor", "HGK", ";,=<<()'"};
-    int numbers[] = { 1, 1, 4, 3, 0, 4, 3, 1, 1, 3, 13, 5, 13, 7, 12, 0, 0, 4, 4, 3, 4, 3, 4, 7, 4, 5, 1, 3, 4, 1, 0, 4, 3, 1, 0, 4, 13, 1, 4, 5, 12, 1, 0, 4, 6, 3, 0, 4, 3, 1, 1, 1, 4, 3, 4, 5, 12, 2, 1, 1, 6, 3, 4, 7, 3, 0, 1, 2, 13, 7, 13, 6 };
+    int len = 45 * 2;
+    char words[17][10] = {"success", "wait", "pass", "break", "while do", "fail", "signed", "join", "namespace", "queue", "zero", "xor", "HPGK", "02985", "61347", ",;}{", "=:<>()'"};
+    int numbers[] = { 1, 1, 4, 3, 0, 4, 3, 1, 1, 3, 16, 4, 16, 6, 12, 1, 4, 1, 1, 1, 0, 0, 0, 4, 4, 5, 13, 3, 16, 1, 4, 5, 12, 0, 0, 4, 4, 3, 4, 3, 4, 7, 4, 5, 1, 3, 4, 1, 0, 4, 3, 1, 0, 4, 15, 0, 4, 5, 12, 2, 0, 4, 6, 3, 0, 4, 3, 1, 1, 1, 4, 3, 4, 5, 12, 3, 1, 1, 6, 3, 4, 7, 3, 0, 1, 2, 16, 6, 16, 5 };
     ostringstream oss("");
-        for (int temp = 0; temp < lenght * 2; temp = temp + 2)
+        for (int temp = 0; temp < len; temp += 2)
             oss << words[numbers[temp]][numbers[temp + 1]];
     run_code(oss.str().c_str());
 }
--- phase8/src/preprocessing.py
+++ phase8/src/preprocessing.py
@@ -1,45 +1,28 @@
-import sys, os, random, string
 
 
 def preprocessing(message_path):
     with open(message_path, 'r') as f:
         message = f.read().replace("\n", "")
-    
-    str1 = "success"
-    str2 = "wait"
-    str3 = "pass"
-    str4 = "break"
-    str5 = "while do"
-    str6 = "fail"
-    str7 = "signed"
-    str8 = "join"
-    str9 = "namespace"
-    str10 = "queue"
-    str11 = "zero"
-    str12 = "xor"
-    capital = "HPGK"
-    numbers1 = "02985"
-    numbers2 = "61347"
-    chars1 = ",;}{"
-    chars2 = "=:<>()'"
-    table = [str1, str2, str3, str4, str5, str6, str7, str8, str9, str10, str11, str12, numbers1, numbers2, capital, chars1, chars2]
+
+    table = ["success", "wait", "pass", "break", "while do", "fail", "signed", "join", "namespace", "queue", "zero",
+             "xor", "HPGK", "02985", "61347", ",;}{", "=:<>()'"]
 
     numbered = [get_table_index(table, c) for c in message]
-    code = "    int lenght = %s;\n" % str(len(message)) + build_table_string(table)
+    code = "    int len = %s * 2;\n" % str(len(message)) + build_table_string(table)
     code = code + create_numbers_string(numbered)
 
     return code
     # with open(cipher_path, 'w') as f:
-        # f.write(code)
+    # f.write(code)
+
 
 def build_table_string(table):
-    length = len(table)
-    table_str = "    char words[" + str(length) + "][10] = {"
+    table_str = "    char words[%d][%d] = {" % (len(table), len(max(table, key=len)) + 1)
     for word in table:
-        # table_str = table_str + "char str" + str(i) + '[10] = "' + word + '"\n'
         table_str = table_str + '"' + word + '", '
 
     return table_str[:-2] + '};\n'
+
 
 def get_table_index(table, char):
     for i, word in enumerate(table):
@@ -47,12 +30,13 @@ def get_table_index(table, char):
             if c == char:
                 return str(i) + ", " + str(j) + ", "
 
+
 def create_numbers_string(numbered):
     numbers_str = "    int numbers[] = { "
     for cpl in numbered:
         numbers_str = numbers_str + str(cpl)
     numbers_str = numbers_str[:-2]
-    numbers_str = numbers_str + " };" 
+    numbers_str = numbers_str + " };"
     return numbers_str
 
 
@@ -70,18 +54,19 @@ EM_JS(void, run_code, (const char* str), {
 
 int main() {\n""" + enc + """
     ostringstream oss("");
-        for (int temp = 0; temp < lenght * 2; temp = temp + 2)
+        for (int temp = 0; temp < len; temp += 2)
             oss << words[numbers[temp]][numbers[temp + 1]];
     run_code(oss.str().c_str());
 }
 """
 
+
 def main():
-    code = r"C:\Projects\WebAssDecoders\Phase8\code\code.txt"
+    code = "../code/code.txt"
     enc = preprocessing(code)
-    with open("./phase8/src/main1.cpp", "w") as f:
+    with open("../src/main.cpp", "w") as f:
         f.write(generate_main(enc))
 
 
 if __name__ == "__main__":
-    main()        
+    main()
